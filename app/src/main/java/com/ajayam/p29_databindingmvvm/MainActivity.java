8e6897cee04b4f90196747fec37142dc81ad2369
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-06-13 11:04:03 +0530

    Initial commit

---
+++ app/src/main/java/com/ajayam/p29_databindingmvvm/MainActivity.java
@@ -0,0 +1,23 @@
+package com.ajayam.p29_databindingmvvm;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import com.ajayam.p29_databindingmvvm.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        ActivityMainBinding mainxml = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        Product p = new Product("Data related form model class");
+        mainxml.setProduct(p);
+
+
+    }
+}
---
+++ app/src/main/java/com/ajayam/p29_databindingmvvm/Product.java
@@ -0,0 +1,18 @@
+package com.ajayam.p29_databindingmvvm;
+
+public class Product {
+
+    String pname;
+
+    public Product(String pname) {
+        this.pname = pname;
+    }
+
+    public String getPname() {
+        return pname;
+    }
+
+    public void setPname(String pname) {
+        this.pname = pname;
+    }
+}
